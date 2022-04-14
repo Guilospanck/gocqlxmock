597b4407d72497dcb291b18af2e602361cffb702
Guilherme Rodrigues <guilherme.rp2010@hotmail.com>
2022-04-14 05:18:48 -0300

    tests: sessionx 100%

--- sessionx_test.go
+++ sessionx_test.go
@@ -2,6 +2,7 @@ package gocqlxmock
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,6 +12,8 @@ type sessionXSut struct {
 	ctx          context.Context
 	stmt         string
 	names        []string
+	err          error
+	errMsg       string
 	sessionxmock *SessionxMock
 	querymock    *QueryxMock
 }
@@ -19,6 +22,8 @@ func makeSessionxSut() sessionXSut {
 	ctx := context.Background()
 	stmt := "statement"
 	names := []string{"name1", "name2"}
+	errMsg := "sessionx_error"
+	err := fmt.Errorf(errMsg)
 
 	sessionxmock := &SessionxMock{}
 	querymock := &QueryxMock{}
@@ -27,6 +32,8 @@ func makeSessionxSut() sessionXSut {
 		ctx,
 		stmt,
 		names,
+		err,
+		errMsg,
 		sessionxmock,
 		querymock,
 	}
@@ -46,5 +53,102 @@ func Test_Sessionx_ContextQuery(t *testing.T) {
 		sut.sessionxmock.AssertCalled(t, "ContextQuery", sut.ctx, sut.stmt, sut.names)
 		sut.sessionxmock.AssertNumberOfCalls(t, "ContextQuery", 1)
 		assert.Equal(t, result, sut.querymock)
+	})
+}
+
+func Test_Sessionx_Query(t *testing.T) {
+	t.Run("Should call Query with proper parameters and return proper result", func(t *testing.T) {
+		// arrange
+		sut := makeSessionxSut()
+		sut.sessionxmock.On("Query", sut.stmt, sut.names).Return(sut.querymock)
+
+		// act
+		result := sut.sessionxmock.Query(sut.stmt, sut.names)
+
+		// assert
+		sut.sessionxmock.AssertExpectations(t)
+		sut.sessionxmock.AssertCalled(t, "Query", sut.stmt, sut.names)
+		sut.sessionxmock.AssertNumberOfCalls(t, "Query", 1)
+		assert.Equal(t, result, sut.querymock)
+	})
+}
+
+func Test_Sessionx_ExecStmt(t *testing.T) {
+	t.Run("Should call ExecStmt with proper parameters and return proper result", func(t *testing.T) {
+		// arrange
+		sut := makeSessionxSut()
+		sut.sessionxmock.On("ExecStmt", sut.stmt).Return(nil)
+
+		// act
+		result := sut.sessionxmock.ExecStmt(sut.stmt)
+
+		// assert
+		sut.sessionxmock.AssertExpectations(t)
+		sut.sessionxmock.AssertCalled(t, "ExecStmt", sut.stmt)
+		sut.sessionxmock.AssertNumberOfCalls(t, "ExecStmt", 1)
+		assert.NoError(t, result)
+	})
+
+	t.Run("Should call ExecStmt with proper parameters and return error", func(t *testing.T) {
+		// arrange
+		sut := makeSessionxSut()
+		sut.sessionxmock.On("ExecStmt", sut.stmt).Return(sut.err)
+
+		// act
+		result := sut.sessionxmock.ExecStmt(sut.stmt)
+
+		// assert
+		sut.sessionxmock.AssertExpectations(t)
+		sut.sessionxmock.AssertCalled(t, "ExecStmt", sut.stmt)
+		sut.sessionxmock.AssertNumberOfCalls(t, "ExecStmt", 1)
+		assert.Error(t, result, sut.errMsg)
+	})
+}
+
+func Test_Sessionx_AwaitSchemaAgreement(t *testing.T) {
+	t.Run("Should call AwaitSchemaAgreement with proper parameters and return proper result", func(t *testing.T) {
+		// arrange
+		sut := makeSessionxSut()
+		sut.sessionxmock.On("AwaitSchemaAgreement", sut.ctx).Return(nil)
+
+		// act
+		result := sut.sessionxmock.AwaitSchemaAgreement(sut.ctx)
+
+		// assert
+		sut.sessionxmock.AssertExpectations(t)
+		sut.sessionxmock.AssertCalled(t, "AwaitSchemaAgreement", sut.ctx)
+		sut.sessionxmock.AssertNumberOfCalls(t, "AwaitSchemaAgreement", 1)
+		assert.NoError(t, result)
+	})
+
+	t.Run("Should call AwaitSchemaAgreement with proper parameters and return error", func(t *testing.T) {
+		// arrange
+		sut := makeSessionxSut()
+		sut.sessionxmock.On("AwaitSchemaAgreement", sut.ctx).Return(sut.err)
+
+		// act
+		result := sut.sessionxmock.AwaitSchemaAgreement(sut.ctx)
+
+		// assert
+		sut.sessionxmock.AssertExpectations(t)
+		sut.sessionxmock.AssertCalled(t, "AwaitSchemaAgreement", sut.ctx)
+		sut.sessionxmock.AssertNumberOfCalls(t, "AwaitSchemaAgreement", 1)
+		assert.Error(t, result, sut.errMsg)
+	})
+}
+
+func Test_Sessionx_Close(t *testing.T) {
+	t.Run("Should call Close with proper parameters and return void", func(t *testing.T) {
+		// arrange
+		sut := makeSessionxSut()
+		sut.sessionxmock.On("Close").Return()
+
+		// act
+		sut.sessionxmock.Close()
+
+		// assert
+		sut.sessionxmock.AssertExpectations(t)
+		sut.sessionxmock.AssertCalled(t, "Close")
+		sut.sessionxmock.AssertNumberOfCalls(t, "Close", 1)
 	})
 }
