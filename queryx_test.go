516b791b493d80d1ce6f1aa575f6add92e9c854c
Guilherme Rodrigues <guilherme.rp2010@hotmail.com>
2022-04-14 14:11:49 -0300

    test: queryx tests

--- queryx_test.go
+++ queryx_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/gocql/gocql"
 	"github.com/scylladb/gocqlx/v2"
@@ -52,6 +53,14 @@ type queryXSut struct {
 	trace       gocql.Tracer
 	observer    gocql.QueryObserver
 	pagesize_n  int
+	timestamp   int64
+	routingKey  []byte
+	ctx         context.Context
+	p           float64
+	retry       gocql.RetryPolicy
+	sp          gocql.SpeculativeExecutionPolicy
+	cons        gocql.SerialConsistency
+	state       []byte
 }
 
 type tracer struct{}
@@ -61,6 +70,16 @@ func (tracer *tracer) Trace(traceId []byte) {}
 type observerstruct struct{}
 
 func (observer *observerstruct) ObserveQuery(context.Context, gocql.ObservedQuery) {}
+
+type retrypolicystruct struct{}
+
+func (rp *retrypolicystruct) Attempt(gocql.RetryableQuery) bool  { return true }
+func (rp *retrypolicystruct) GetRetryType(error) gocql.RetryType { return gocql.RetryType(10) }
+
+type spstruct struct{}
+
+func (sp *spstruct) Attempts() int        { return 1 }
+func (sp *spstruct) Delay() time.Duration { return time.Duration(1) }
 
 func makeQueryxSut() queryXSut {
 	tr := func(name string, val interface{}) interface{} { return nil }
@@ -73,6 +92,14 @@ func makeQueryxSut() queryXSut {
 	var trace gocql.Tracer = &tracer{}
 	var observer gocql.QueryObserver = &observerstruct{}
 	pagesize_n := 10
+	timestamp := int64(10)
+	routingKey := []byte{}
+	ctx := context.Background()
+	p := float64(10.1)
+	retry := &retrypolicystruct{}
+	sp := &spstruct{}
+	cons := gocql.SerialConsistency(10)
+	state := []byte{}
 
 	return queryXSut{
 		tr,
@@ -85,6 +112,14 @@ func makeQueryxSut() queryXSut {
 		trace,
 		observer,
 		pagesize_n,
+		timestamp,
+		routingKey,
+		ctx,
+		p,
+		retry,
+		sp,
+		cons,
+		state,
 	}
 }
 
@@ -616,5 +651,276 @@ func Test_Queryx_Trace(t *testing.T) {
 		sut.queryxmock.AssertCalled(t, "Trace", sut.trace)
 		sut.queryxmock.AssertNumberOfCalls(t, "Trace", 1)
 		assert.Equal(t, sut.queryxmock, result)
+	})
+}
+
+func Test_Queryx_Observer(t *testing.T) {
+	t.Run("Should call Observer with proper parameters and return proper result", func(t *testing.T) {
+		// arrange
+		sut := makeQueryxSut()
+		sut.queryxmock.On("Observer", sut.observer).Return(sut.queryxmock)
+
+		// act
+		result := sut.queryxmock.Observer(sut.observer)
+
+		// assert
+		sut.queryxmock.AssertExpectations(t)
+		sut.queryxmock.AssertCalled(t, "Observer", sut.observer)
+		sut.queryxmock.AssertNumberOfCalls(t, "Observer", 1)
+		assert.Equal(t, sut.queryxmock, result)
+	})
+}
+
+func Test_Queryx_PageSize(t *testing.T) {
+	t.Run("Should call PageSize with proper parameters and return proper result", func(t *testing.T) {
+		// arrange
+		sut := makeQueryxSut()
+		sut.queryxmock.On("PageSize", sut.pagesize_n).Return(sut.queryxmock)
+
+		// act
+		result := sut.queryxmock.PageSize(sut.pagesize_n)
+
+		// assert
+		sut.queryxmock.AssertExpectations(t)
+		sut.queryxmock.AssertCalled(t, "PageSize", sut.pagesize_n)
+		sut.queryxmock.AssertNumberOfCalls(t, "PageSize", 1)
+		assert.Equal(t, sut.queryxmock, result)
+	})
+}
+
+func Test_Queryx_DefaultTimestamp(t *testing.T) {
+	t.Run("Should call DefaultTimestamp with proper parameters and return proper result", func(t *testing.T) {
+		// arrange
+		sut := makeQueryxSut()
+		sut.queryxmock.On("DefaultTimestamp", sut.boolVar).Return(sut.queryxmock)
+
+		// act
+		result := sut.queryxmock.DefaultTimestamp(sut.boolVar)
+
+		// assert
+		sut.queryxmock.AssertExpectations(t)
+		sut.queryxmock.AssertCalled(t, "DefaultTimestamp", sut.boolVar)
+		sut.queryxmock.AssertNumberOfCalls(t, "DefaultTimestamp", 1)
+		assert.Equal(t, sut.queryxmock, result)
+	})
+}
+
+func Test_Queryx_WithTimestamp(t *testing.T) {
+	t.Run("Should call WithTimestamp with proper parameters and return proper result", func(t *testing.T) {
+		// arrange
+		sut := makeQueryxSut()
+		sut.queryxmock.On("WithTimestamp", sut.timestamp).Return(sut.queryxmock)
+
+		// act
+		result := sut.queryxmock.WithTimestamp(sut.timestamp)
+
+		// assert
+		sut.queryxmock.AssertExpectations(t)
+		sut.queryxmock.AssertCalled(t, "WithTimestamp", sut.timestamp)
+		sut.queryxmock.AssertNumberOfCalls(t, "WithTimestamp", 1)
+		assert.Equal(t, sut.queryxmock, result)
+	})
+}
+
+func Test_Queryx_RoutingKey(t *testing.T) {
+	t.Run("Should call RoutingKey with proper parameters and return proper result", func(t *testing.T) {
+		// arrange
+		sut := makeQueryxSut()
+		sut.queryxmock.On("RoutingKey", sut.routingKey).Return(sut.queryxmock)
+
+		// act
+		result := sut.queryxmock.RoutingKey(sut.routingKey)
+
+		// assert
+		sut.queryxmock.AssertExpectations(t)
+		sut.queryxmock.AssertCalled(t, "RoutingKey", sut.routingKey)
+		sut.queryxmock.AssertNumberOfCalls(t, "RoutingKey", 1)
+		assert.Equal(t, sut.queryxmock, result)
+	})
+}
+
+func Test_Queryx_WithContext(t *testing.T) {
+	t.Run("Should call WithContext with proper parameters and return proper result", func(t *testing.T) {
+		// arrange
+		sut := makeQueryxSut()
+		sut.queryxmock.On("WithContext", sut.ctx).Return(sut.queryxmock)
+
+		// act
+		result := sut.queryxmock.WithContext(sut.ctx)
+
+		// assert
+		sut.queryxmock.AssertExpectations(t)
+		sut.queryxmock.AssertCalled(t, "WithContext", sut.ctx)
+		sut.queryxmock.AssertNumberOfCalls(t, "WithContext", 1)
+		assert.Equal(t, sut.queryxmock, result)
+	})
+}
+
+func Test_Queryx_Prefetch(t *testing.T) {
+	t.Run("Should call Prefetch with proper parameters and return proper result", func(t *testing.T) {
+		// arrange
+		sut := makeQueryxSut()
+		sut.queryxmock.On("Prefetch", sut.p).Return(sut.queryxmock)
+
+		// act
+		result := sut.queryxmock.Prefetch(sut.p)
+
+		// assert
+		sut.queryxmock.AssertExpectations(t)
+		sut.queryxmock.AssertCalled(t, "Prefetch", sut.p)
+		sut.queryxmock.AssertNumberOfCalls(t, "Prefetch", 1)
+		assert.Equal(t, sut.queryxmock, result)
+	})
+}
+
+func Test_Queryx_RetryPolicy(t *testing.T) {
+	t.Run("Should call RetryPolicy with proper parameters and return proper result", func(t *testing.T) {
+		// arrange
+		sut := makeQueryxSut()
+		sut.queryxmock.On("RetryPolicy", sut.retry).Return(sut.queryxmock)
+
+		// act
+		result := sut.queryxmock.RetryPolicy(sut.retry)
+
+		// assert
+		sut.queryxmock.AssertExpectations(t)
+		sut.queryxmock.AssertCalled(t, "RetryPolicy", sut.retry)
+		sut.queryxmock.AssertNumberOfCalls(t, "RetryPolicy", 1)
+		assert.Equal(t, sut.queryxmock, result)
+	})
+}
+
+func Test_Queryx_SetSpeculativeExecutionPolicy(t *testing.T) {
+	t.Run("Should call SetSpeculativeExecutionPolicy with proper parameters and return proper result", func(t *testing.T) {
+		// arrange
+		sut := makeQueryxSut()
+		sut.queryxmock.On("SetSpeculativeExecutionPolicy", sut.sp).Return(sut.queryxmock)
+
+		// act
+		result := sut.queryxmock.SetSpeculativeExecutionPolicy(sut.sp)
+
+		// assert
+		sut.queryxmock.AssertExpectations(t)
+		sut.queryxmock.AssertCalled(t, "SetSpeculativeExecutionPolicy", sut.sp)
+		sut.queryxmock.AssertNumberOfCalls(t, "SetSpeculativeExecutionPolicy", 1)
+		assert.Equal(t, sut.queryxmock, result)
+	})
+}
+
+func Test_Queryx_Idempotent(t *testing.T) {
+	t.Run("Should call Idempotent with proper parameters and return proper result", func(t *testing.T) {
+		// arrange
+		sut := makeQueryxSut()
+		sut.queryxmock.On("Idempotent", sut.boolVar).Return(sut.queryxmock)
+
+		// act
+		result := sut.queryxmock.Idempotent(sut.boolVar)
+
+		// assert
+		sut.queryxmock.AssertExpectations(t)
+		sut.queryxmock.AssertCalled(t, "Idempotent", sut.boolVar)
+		sut.queryxmock.AssertNumberOfCalls(t, "Idempotent", 1)
+		assert.Equal(t, sut.queryxmock, result)
+	})
+}
+
+func Test_Queryx_SerialConsistency(t *testing.T) {
+	t.Run("Should call SerialConsistency with proper parameters and return proper result", func(t *testing.T) {
+		// arrange
+		sut := makeQueryxSut()
+		sut.queryxmock.On("SerialConsistency", sut.cons).Return(sut.queryxmock)
+
+		// act
+		result := sut.queryxmock.SerialConsistency(sut.cons)
+
+		// assert
+		sut.queryxmock.AssertExpectations(t)
+		sut.queryxmock.AssertCalled(t, "SerialConsistency", sut.cons)
+		sut.queryxmock.AssertNumberOfCalls(t, "SerialConsistency", 1)
+		assert.Equal(t, sut.queryxmock, result)
+	})
+}
+
+func Test_Queryx_PageState(t *testing.T) {
+	t.Run("Should call PageState with proper parameters and return proper result", func(t *testing.T) {
+		// arrange
+		sut := makeQueryxSut()
+		sut.queryxmock.On("PageState", sut.state).Return(sut.queryxmock)
+
+		// act
+		result := sut.queryxmock.PageState(sut.state)
+
+		// assert
+		sut.queryxmock.AssertExpectations(t)
+		sut.queryxmock.AssertCalled(t, "PageState", sut.state)
+		sut.queryxmock.AssertNumberOfCalls(t, "PageState", 1)
+		assert.Equal(t, sut.queryxmock, result)
+	})
+}
+
+func Test_Queryx_NoSkipMetadata(t *testing.T) {
+	t.Run("Should call NoSkipMetadata with proper parameters and return proper result", func(t *testing.T) {
+		// arrange
+		sut := makeQueryxSut()
+		sut.queryxmock.On("NoSkipMetadata").Return(sut.queryxmock)
+
+		// act
+		result := sut.queryxmock.NoSkipMetadata()
+
+		// assert
+		sut.queryxmock.AssertExpectations(t)
+		sut.queryxmock.AssertCalled(t, "NoSkipMetadata")
+		sut.queryxmock.AssertNumberOfCalls(t, "NoSkipMetadata", 1)
+		assert.Equal(t, sut.queryxmock, result)
+	})
+}
+
+func Test_Queryx_Release(t *testing.T) {
+	t.Run("Should call Release with proper parameters and return proper result", func(t *testing.T) {
+		// arrange
+		sut := makeQueryxSut()
+		sut.queryxmock.On("Release").Return(sut.queryxmock)
+
+		// act
+		sut.queryxmock.Release()
+
+		// assert
+		sut.queryxmock.AssertExpectations(t)
+		sut.queryxmock.AssertCalled(t, "Release")
+		sut.queryxmock.AssertNumberOfCalls(t, "Release", 1)
+	})
+}
+
+func Test_Queryx_Scan(t *testing.T) {
+	t.Run("Should call Scan with proper parameters and return proper result", func(t *testing.T) {
+		// arrange
+		sut := makeQueryxSut()
+		arg := makeArg("potato")
+		sut.queryxmock.On("Scan", arg).Return(nil)
+
+		// act
+		err := sut.queryxmock.Scan(arg)
+
+		// assert
+		sut.queryxmock.AssertExpectations(t)
+		sut.queryxmock.AssertCalled(t, "Scan", arg)
+		sut.queryxmock.AssertNumberOfCalls(t, "Scan", 1)
+		assert.NoError(t, err)
+	})
+
+	t.Run("Should call Scan with proper parameters and return proper result", func(t *testing.T) {
+		// arrange
+		sut := makeQueryxSut()
+		arg := makeArg("potato")
+		sut.queryxmock.On("Scan", arg).Return(sut.err)
+
+		// act
+		err := sut.queryxmock.Scan(arg)
+
+		// assert
+		sut.queryxmock.AssertExpectations(t)
+		sut.queryxmock.AssertCalled(t, "Scan", arg)
+		sut.queryxmock.AssertNumberOfCalls(t, "Scan", 1)
+		assert.Error(t, err, sut.errMsg)
 	})
 }
